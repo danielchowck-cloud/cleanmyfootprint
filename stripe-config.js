a642401117e1d5bf0934f479778c2c5f48fad8df
Daniel <dcck888@Daniels-MacBook-Neo.local>
2026-06-11 16:06:52 +0800

    Activate CleanMyFootprint Stripe link

--- stripe-config.js
+++ stripe-config.js
@@ -1,5 +1,5 @@
 window.CMF_STRIPE_CONFIG = {
-  paymentLink: "",
+  paymentLink: "https://buy.stripe.com/eVqcN6d6j5tb67b8G21RC00",
   successUrl: "payment-success.html",
   intakeUrl: "intake.html",
   productName: "Personal Online Footprint Scan",
